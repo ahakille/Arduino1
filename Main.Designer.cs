3f95cc03b876d3124c4512913b343fa4bd6614a3
Nicklas Persson <gorlingy@hotmail.com>
2017-07-29 09:56:08 +0200

    fix

---
+++ Main.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Arduino1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntest = new System.Windows.Forms.Button();
+            this.btnAv = new System.Windows.Forms.Button();
+            this.lbtext = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.vxl2höger = new System.Windows.Forms.Button();
+            this.vl2btnrak = new System.Windows.Forms.Button();
+            this.vxl2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.vxl1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.vxl4 = new System.Windows.Forms.PictureBox();
+            this.vxl3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.vxl4t = new System.Windows.Forms.Button();
+            this.vxl4f = new System.Windows.Forms.Button();
+            this.vxl3t = new System.Windows.Forms.Button();
+            this.vxl3f = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.vxl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vxl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vxl4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vxl3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btntest
+            // 
+            this.btntest.Location = new System.Drawing.Point(48, 513);
+            this.btntest.Name = "btntest";
+            this.btntest.Size = new System.Drawing.Size(75, 23);
+            this.btntest.TabIndex = 0;
+            this.btntest.Text = "På";
+            this.btntest.UseVisualStyleBackColor = true;
+            this.btntest.Click += new System.EventHandler(this.btntest_Click);
+            // 
+            // btnAv
+            // 
+            this.btnAv.Location = new System.Drawing.Point(129, 513);
+            this.btnAv.Name = "btnAv";
+            this.btnAv.Size = new System.Drawing.Size(75, 23);
+            this.btnAv.TabIndex = 0;
+            this.btnAv.Text = "Av";
+            this.btnAv.UseVisualStyleBackColor = true;
+            this.btnAv.Click += new System.EventHandler(this.btnAv_Click);
+            // 
+            // lbtext
+            // 
+            this.lbtext.AutoSize = true;
+            this.lbtext.Location = new System.Drawing.Point(106, 489);
+            this.lbtext.Name = "lbtext";
+            this.lbtext.Size = new System.Drawing.Size(0, 17);
+            this.lbtext.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(46, 31);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // vxl2höger
+            // 
+            this.vxl2höger.Location = new System.Drawing.Point(770, 509);
+            this.vxl2höger.Name = "vxl2höger";
+            this.vxl2höger.Size = new System.Drawing.Size(75, 23);
+            this.vxl2höger.TabIndex = 0;
+            this.vxl2höger.Text = "På";
+            this.vxl2höger.UseVisualStyleBackColor = true;
+            this.vxl2höger.Click += new System.EventHandler(this.vxl2höger_Click);
+            // 
+            // vl2btnrak
+            // 
+            this.vl2btnrak.Location = new System.Drawing.Point(851, 509);
+            this.vl2btnrak.Name = "vl2btnrak";
+            this.vl2btnrak.Size = new System.Drawing.Size(75, 23);
+            this.vl2btnrak.TabIndex = 0;
+            this.vl2btnrak.Text = "Av";
+            this.vl2btnrak.UseVisualStyleBackColor = true;
+            this.vl2btnrak.Click += new System.EventHandler(this.vl2btnrak_Click);
+            // 
+            // vxl2
+            // 
+            this.vxl2.Image = global::Arduino1.Properties.Resources._2raka;
+            this.vxl2.Location = new System.Drawing.Point(757, 413);
+            this.vxl2.Name = "vxl2";
+            this.vxl2.Size = new System.Drawing.Size(190, 70);
+            this.vxl2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.vxl2.TabIndex = 2;
+            this.vxl2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Arduino1.Properties.Resources._2raka;
+            this.pictureBox1.Location = new System.Drawing.Point(234, 413);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // vxl1
+            // 
+            this.vxl1.Image = global::Arduino1.Properties.Resources._2raka;
+            this.vxl1.Location = new System.Drawing.Point(48, 413);
+            this.vxl1.Name = "vxl1";
+            this.vxl1.Size = new System.Drawing.Size(190, 70);
+            this.vxl1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.vxl1.TabIndex = 2;
+            this.vxl1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Arduino1.Properties.Resources._2raka;
+            this.pictureBox2.Location = new System.Drawing.Point(419, 413);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(190, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Arduino1.Properties.Resources._2raka;
+            this.pictureBox3.Location = new System.Drawing.Point(568, 413);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(190, 70);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // vxl4
+            // 
+            this.vxl4.Image = global::Arduino1.Properties.Resources._2raka;
+            this.vxl4.Location = new System.Drawing.Point(573, 371);
+            this.vxl4.Name = "vxl4";
+            this.vxl4.Size = new System.Drawing.Size(183, 69);
+            this.vxl4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.vxl4.TabIndex = 2;
+            this.vxl4.TabStop = false;
+            // 
+            // vxl3
+            // 
+            this.vxl3.Image = global::Arduino1.Properties.Resources._2raka1;
+            this.vxl3.Location = new System.Drawing.Point(234, 371);
+            this.vxl3.Name = "vxl3";
+            this.vxl3.Size = new System.Drawing.Size(179, 69);
+            this.vxl3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.vxl3.TabIndex = 2;
+            this.vxl3.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Arduino1.Properties.Resources._2raka;
+            this.pictureBox6.Location = new System.Drawing.Point(409, 371);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(190, 70);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 2;
+            this.pictureBox6.TabStop = false;
+            // 
+            // vxl4t
+            // 
+            this.vxl4t.Location = new System.Drawing.Point(596, 509);
+            this.vxl4t.Name = "vxl4t";
+            this.vxl4t.Size = new System.Drawing.Size(75, 23);
+            this.vxl4t.TabIndex = 0;
+            this.vxl4t.Text = "På";
+            this.vxl4t.UseVisualStyleBackColor = true;
+            this.vxl4t.Click += new System.EventHandler(this.vxl4t_Click);
+            // 
+            // vxl4f
+            // 
+            this.vxl4f.Location = new System.Drawing.Point(677, 509);
+            this.vxl4f.Name = "vxl4f";
+            this.vxl4f.Size = new System.Drawing.Size(75, 23);
+            this.vxl4f.TabIndex = 0;
+            this.vxl4f.Text = "Av";
+            this.vxl4f.UseVisualStyleBackColor = true;
+            this.vxl4f.Click += new System.EventHandler(this.vxl4f_Click);
+            // 
+            // vxl3t
+            // 
+            this.vxl3t.Location = new System.Drawing.Point(234, 513);
+            this.vxl3t.Name = "vxl3t";
+            this.vxl3t.Size = new System.Drawing.Size(75, 23);
+            this.vxl3t.TabIndex = 0;
+            this.vxl3t.Text = "På";
+            this.vxl3t.UseVisualStyleBackColor = true;
+            this.vxl3t.Click += new System.EventHandler(this.vxl3t_Click);
+            // 
+            // vxl3f
+            // 
+            this.vxl3f.Location = new System.Drawing.Point(315, 513);
+            this.vxl3f.Name = "vxl3f";
+            this.vxl3f.Size = new System.Drawing.Size(75, 23);
+            this.vxl3f.TabIndex = 0;
+            this.vxl3f.Text = "Av";
+            this.vxl3f.UseVisualStyleBackColor = true;
+            this.vxl3f.Click += new System.EventHandler(this.vxl3f_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1366, 618);
+            this.Controls.Add(this.vxl2);
+            this.Controls.Add(this.vxl3);
+            this.Controls.Add(this.vxl4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.vxl1);
+            this.Controls.Add(this.lbtext);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.vxl3f);
+            this.Controls.Add(this.vxl4f);
+            this.Controls.Add(this.vl2btnrak);
+            this.Controls.Add(this.vxl3t);
+            this.Controls.Add(this.vxl4t);
+            this.Controls.Add(this.vxl2höger);
+            this.Controls.Add(this.btnAv);
+            this.Controls.Add(this.btntest);
+            this.Controls.Add(this.pictureBox6);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vxl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vxl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vxl4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vxl3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntest;
+        private System.Windows.Forms.Button btnAv;
+        private System.Windows.Forms.Label lbtext;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox vxl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox vxl2;
+        private System.Windows.Forms.Button vxl2höger;
+        private System.Windows.Forms.Button vl2btnrak;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox vxl4;
+        private System.Windows.Forms.PictureBox vxl3;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button vxl4t;
+        private System.Windows.Forms.Button vxl4f;
+        private System.Windows.Forms.Button vxl3t;
+        private System.Windows.Forms.Button vxl3f;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,96 @@
+﻿using ArduinoDriver.SerialProtocol;
+using ArduinoUploader.Hardware;
+using System;
+using System.Windows.Forms;
+
+namespace Arduino1
+{
+    public partial class Main : Form
+    {
+        ArduinoDriver.ArduinoDriver driver = new ArduinoDriver.ArduinoDriver(ArduinoModel.UnoR3, "COM3", true);
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btntest_Click(object sender, EventArgs e)
+        {
+            //driver.Send(new (,ArduinoDriver.DigitalValue.High));
+            driver.Send(new DigitalWriteRequest(3, ArduinoDriver.DigitalValue.High));
+            //driver.Send(new AnalogWriteRequest(3, 1));
+            var test = driver.Send(new DigitalReadRequest(3));
+            lbtext.Text = test.PinValue.ToString();
+            vxl1.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2vänster.png";
+
+        }
+
+        private void btnAv_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(3, ArduinoDriver.DigitalValue.Low));
+           
+            var test = driver.Send(new DigitalReadRequest(3));
+            lbtext.Text = test.PinValue.ToString();
+            vxl1.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2raka.png";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(3, ArduinoDriver.DigitalValue.High));
+            driver.Send(new AnalogWriteRequest(3, 1));
+            driver.Send(new DigitalWriteRequest(3, ArduinoDriver.DigitalValue.Low));
+            driver.Send(new DigitalWriteRequest(4, ArduinoDriver.DigitalValue.High));
+            driver.Send(new AnalogWriteRequest(4, 1));
+            driver.Send(new DigitalWriteRequest(4, ArduinoDriver.DigitalValue.Low));
+            driver.Send(new DigitalWriteRequest(5, ArduinoDriver.DigitalValue.High));
+            driver.Send(new AnalogWriteRequest(5, 1));
+            driver.Send(new DigitalWriteRequest(5, ArduinoDriver.DigitalValue.Low));
+            driver.Send(new DigitalWriteRequest(6, ArduinoDriver.DigitalValue.High));
+            driver.Send(new AnalogWriteRequest(6, 1));
+            driver.Send(new DigitalWriteRequest(6, ArduinoDriver.DigitalValue.Low));
+        }
+
+        private void vxl2höger_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(4, ArduinoDriver.DigitalValue.High));
+            vxl2.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2höger.png";
+        }
+
+        private void vl2btnrak_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(4, ArduinoDriver.DigitalValue.Low));
+            vxl2.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2raka.png";
+        }
+        private void vxl3h_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(4, ArduinoDriver.DigitalValue.Low));
+            
+            pictureBox2.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2raka.png";
+        }
+        private void vxl3t_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(5, ArduinoDriver.DigitalValue.High));
+            vxl3.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2vänster.png";
+        }
+
+        private void vxl3f_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(5, ArduinoDriver.DigitalValue.Low));
+
+            vxl3.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2raka.png";
+        }
+
+        private void vxl4t_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(6, ArduinoDriver.DigitalValue.High));
+            vxl4.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2höger.png";
+        }
+
+        private void vxl4f_Click(object sender, EventArgs e)
+        {
+            driver.Send(new DigitalWriteRequest(6, ArduinoDriver.DigitalValue.Low));
+
+            vxl4.ImageLocation = @"C:\Users\ahakille\Documents\visual studio 2017\Projects\Arduino1\Resources\2raka.png";
+        }
+    }
+    
+}
